36fb79c555eb6b8127248749e78f910ce01817f8
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-08-02 15:30:05 +0900

    Start Practice

---
+++ src/main/java/com/openbanking/controller/OpenBankingController.java
@@ -0,0 +1,14 @@
+package com.openbanking.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenBankingController {
+	
+	@PostMapping("/authorize")
+	public void authorize() {
+		
+	}
+
+}
