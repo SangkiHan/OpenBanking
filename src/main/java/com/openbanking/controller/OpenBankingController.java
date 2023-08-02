d48296a03d9b03297cbcc4eb888248814437c75b
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-08-02 15:33:55 +0900

    feat: feign의존성 주입

--- src/main/java/com/openbanking/controller/OpenBankingController.java
+++ src/main/java/com/openbanking/controller/OpenBankingController.java
@@ -1,9 +1,11 @@
 package com.openbanking.controller;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableFeignClients
 public class OpenBankingController {
 	
 	@PostMapping("/authorize")
